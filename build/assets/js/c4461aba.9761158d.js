37668800a4980f25d3f0d044a66007961b6a574d
Synthranger <synthrangerofficial@gmail.com>
2022-06-22 13:50:18 +0800

    Moonwave Build

---
+++ build/assets/js/c4461aba.9761158d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[628],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/SimpleFlare","label":"\u200bSimple\u200bFlare"}]')}}]);
